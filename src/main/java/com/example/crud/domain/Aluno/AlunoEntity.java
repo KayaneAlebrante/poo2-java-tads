18bf483141c6921c90bcb92f37882f29b9fb6702
KayaneAlebrante <107808062+KayaneAlebrante@users.noreply.github.com>
2023-10-03 16:08:16 -0300

    Feat: AlunoEntity

---
+++ src/main/java/com/example/crud/domain/Aluno/AlunoEntity.java
@@ -0,0 +1,51 @@
+package com.example.crud.domain.Aluno;
+
+import java.sql.Date;
+
+import com.example.crud.domain.pessoa.PessoaEntity;
+
+public class AlunoEntity extends PessoaEntity{
+    private String email;
+    private Integer turma;
+
+    public AlunoEntity(String nome, Date data, Integer matricula, String email, Integer turma) {
+        super();
+        this.email = email;
+        this.turma = turma;
+    }
+
+    @Override
+    public void exibirDados() {
+        super.exibirDados();
+        System.out.println("Email: " + email);
+        System.out.println("Turma: " + turma);
+    }
+
+    @Override
+    public void cadastrarDados(String nome, Date dataNascimento, Integer matricula) {
+        super.cadastrarDados(nome, dataNascimento, matricula);
+        this.email = ""; /
+        this.turma = 0; 
+    }
+
+    @Override
+    public void editarDados(String nome, Date dataNascimento, Integer matricula) {
+        super.editarDados(nome, dataNascimento, matricula);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Integer turma) {
+        this.turma = turma;
+    }
+}
