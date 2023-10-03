02e562583d009b0637d396f9ad62faf978975414
Lee Eros Galaad Dias de Moures <leeeros@protonmail.com>
2023-10-03 16:03:07 -0300

    Feat: Classe Pessoa

---
+++ src/main/java/com/example/crud/domain/pessoa/PessoaEntity.java
@@ -0,0 +1,36 @@
+package com.example.crud.domain.pessoa;
+
+import java.sql.Date;
+
+public class PessoaEntity {
+    private Long idPessoa;
+    protected String nomeCompleto;
+    private Date dataNascimento;
+    private Integer matricula;
+
+    public void Pessoa(String nome, Date data, Integer matricula) {
+        this.nomeCompleto = nome;
+        this.dataNascimento = data;
+        this.matricula = matricula;
+    }
+
+    public void exibirDados() {
+        System.out.println("ID: " + idPessoa);
+        System.out.println("Nome: " + nomeCompleto);
+        System.out.println("Data de Nascimento: " + dataNascimento);
+        System.out.println("Matrícula: " + matricula);
+    }
+
+    public void cadastrarDados(String nome, Date dataNascimento, Integer matricula) {
+        this.nomeCompleto = nome;
+        this.dataNascimento = dataNascimento;
+        this.matricula = matricula;
+    }
+
+    public void editarDados(String nome, Date dataNascimento, Integer matricula) {
+        this.nomeCompleto = nome;
+        this.dataNascimento = dataNascimento;
+        this.matricula = matricula;
+    }
+    
+}
